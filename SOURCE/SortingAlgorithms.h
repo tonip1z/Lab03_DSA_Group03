73f137e187ddaa7843692b1d4ec8da6e5de45649
tonip1z <95369761+tonip1z@users.noreply.github.com>
2022-11-08 11:32:58 +0700

    final

--- SOURCE/SortingAlgorithms.h
+++ SOURCE/SortingAlgorithms.h
@@ -7,44 +7,44 @@
 
 //Function prototypes for each sorting algorithm goes here
 //1. SelectionSort
-void SelectionSort(int* a, int n, long long &num_Comp);
+void SelectionSort(int* a, int n, unsigned long long &num_Comp);
 
 //2. InsertionSort
-void InsertionSort(int* a, int n, long long &num_Comp);
+void InsertionSort(int* a, int n, unsigned long long &num_Comp);
 
 //3. BubbleSort
-void BubbleSort(int* a, int n, long long &num_Comp);
-void BubbleSort_with_flag(int* a, int n, long long &num_Comp);
+void BubbleSort(int* a, int n, unsigned long long &num_Comp);
+void BubbleSort_with_flag(int* a, int n, unsigned long long &num_Comp);
 
 //4. HeapSort
-void HeapSort(int* a, int n, long long &num_Comp);
-void HeapConstruct(int* a, int n, long long &num_Comp);
-void HeapRebuild(int index, int* a, int n, long long &num_Comp);
+void HeapSort(int* a, int n, unsigned long long &num_Comp);
+void HeapConstruct(int* a, int n, unsigned long long &num_Comp);
+void HeapRebuild(int index, int* a, int n, unsigned long long &num_Comp);
 
 //5. MergeSort
-void MergeSort(int* a, int n, long long &num_Comp);
-void MergeRun(int* a, int temp[], int p, int t, int q, long long &num_Comp);
-void Sort_on_Run(int* a,int temp[],int p, int q, long long &num_Comp);
+void MergeSort(int* a, int n, unsigned long long &num_Comp);
+void MergeRun(int* a, int temp[], int p, int t, int q, unsigned long long &num_Comp);
+void Sort_on_Run(int* a,int temp[],int p, int q, unsigned long long &num_Comp);
 
 //6. QuickSort
-void QuickSort(int* a, int n, long long &num_Comp);
+void QuickSort(int* a, int n, unsigned long long &num_Comp);
 
 //7. RadixSort
-void RadixSort(int* a, int n, long long &num_Comp);
+void RadixSort(int* a, int n, unsigned long long &num_Comp);
 
 //8. ShakerSort
-void ShakerSort(int* a, int n, long long &num_Comp);
+void ShakerSort(int* a, int n, unsigned long long &num_Comp);
 
 //9. ShellSort
-void ShellSort(int* a, int n, long long &num_Comp);
+void ShellSort(int* a, int n, unsigned long long &num_Comp);
 
 //10. CountingSort
-void CountingSort(int* a, int n, long long &num_Comp);
+void CountingSort(int* a, int n, unsigned long long &num_Comp);
 
 //11. FlashSort
-void FlashSort(int* a, int n, long long &num_Comp);
-void FlashInsertionSort(int* a, int n, long long &num_Comp);       //Temporary
-int GetClass(int value, int minValue, int maxValue, int m, long long &num_Comp);
-void ClassPermute(int* a, int n, int minValue, int maxValue, int freq[], int m, long long &num_Comp);
-void ClassSort(int* a, int n, int freq[], int m, long long &num_Comp);
-void Classify(int* a, int n, int& minValue, int& maxValue, int freq[], int m, long long &num_Comp);
+void FlashSort(int* a, int n, unsigned long long &num_Comp);
+void FlashInsertionSort(int* a, int n, unsigned long long &num_Comp);       //Temporary
+int GetClass(int value, int minValue, int maxValue, int m, unsigned long long &num_Comp);
+void ClassPermute(int* a, int n, int minValue, int maxValue, int freq[], int m, unsigned long long &num_Comp);
+void ClassSort(int* a, int n, int freq[], int m, unsigned long long &num_Comp);
+void Classify(int* a, int n, int& minValue, int& maxValue, int freq[], int m, unsigned long long &num_Comp);
